c9ef555125235898befca96417c8a57d8e3f581b
Valentin <59359218+ChristopherStuff@users.noreply.github.com>
2020-07-13 22:45:34 +0300

    update site

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+var options = {
+    loop: true,
+    typeSpeed: 60,
+    backSpeed: 70,
+    backDelay: 900,
+    showCursor: true,
+    startDelay: 90,
+    strings: ["Надо придум", "Надо сюда норм текст", "Developer - christopher stuff.#9999", "SpongeBot"]
+}
+var typed = new Typed(".commands", options);
